159f83f9396ca1d1754869340941b0ec7ce0a524
stevewitman <stevewitman@gmail.com>
2018-11-22 23:46:30 -0700

    Working drag and drop example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'try-md-draganddrop';
+
+  numbers: number[] = [1, 22, 333, 4444, 55555];
+  otherNumbers: number[] = [666666, 7777777, 88888888, 999999999];
+
+  drop(event: CdkDragDrop<number[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      moveItemInArray(this.numbers, event.previousIndex, event.currentIndex);
+    }
+  }
 }
